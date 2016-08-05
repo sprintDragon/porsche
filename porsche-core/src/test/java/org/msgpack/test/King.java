8b4eecb819367e35b2c9090d3fc698f277c89baa
stereo <66017198@qq.com>
2016-08-05 17:35:32 +0800

    2016.8.5 17:35 liujing msgpack vs hessian 性能测试

--- porsche-core/src/test/java/org/msgpack/test/King.java
+++ porsche-core/src/test/java/org/msgpack/test/King.java
@@ -2,11 +2,13 @@ package org.msgpack.test;
 
 import org.msgpack.annotation.Message;
 
+import java.io.Serializable;
+
 /**
  * Created by stereo on 16-8-5.
  */
 @Message
-public class King {
+public class King implements Serializable{
 
     public String jj = "ll";
 }
--- porsche-core/src/test/java/org/msgpack/test/UserInfo.java
+++ porsche-core/src/test/java/org/msgpack/test/UserInfo.java
@@ -2,12 +2,17 @@ package org.msgpack.test;
 
 import org.msgpack.annotation.Message;
 
+import java.io.Serializable;
+
 /**
  * Created by stereo on 16-8-5.
  */
 @Message
-public class UserInfo {
+public class UserInfo implements Serializable{
+
     public long s = 100;
 
     public King king = new King();
+
+    public Object object = new String("hellow");
 }
